3b9a39f902b4fc7ecaa909836fb8cabb8ca332a8
aysanz <aysanzeinalvand@gmail.com>
2024-08-16 02:51:36 +0330

    chore: configure ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {},
+};
